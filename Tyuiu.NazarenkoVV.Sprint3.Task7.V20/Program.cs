250bfe2df0bef1ad674d8e0d6c716044df66f9de
nazarenkovv <vitanazarenko2006@yandex.ru>
2024-11-28 12:13:20 +0500

    Task7

---
+++ Tyuiu.NazarenkoVV.Sprint3.Task7.V20/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.NazarenkoVV.Sprint3.Task7.V20.Lib;
+
+namespace Tyuiu.NazarenkoVV.Sprint3.Task7.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2| Выполнил:  Назаренко В.В.| ";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: |   Назаренко В. В.                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет, и печатает результат на экране                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+            double[] valueArray;
+            valueArray = new double[len];
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Результат:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine(" +-----------+-----------+");
+            Console.WriteLine(" |     X     |    F(x)   |");
+            Console.WriteLine(" +-----------+-----------+");
+            for (int i = 0; i <= len-1; i++ )
+            {
+                Console.WriteLine(" |{0,5:d}       |  {1, 5:f2}   |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine(" +-----------+-----------+");
+            Console.ReadKey();
+        }
+    }
+
+}
+    
